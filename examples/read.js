78d8087f82bc348087a8d099892a5edc93402d86
Don Coleman <dcoleman@chariotsolutions.com>
2015-06-08 20:32:52 -0400

    node 0.12 buffer

--- examples/read.js
+++ examples/read.js
@@ -2,17 +2,23 @@
 
 var ndef = require('ndef'),
     mifare = require('..');
-    
+
 mifare.read(function(err, buffer) {
     if (err) {
         console.log("Read failed ");
         console.log(err);
     } else {
-        // TODO handle empty buffer!
-        var message = ndef.decodeMessage(buffer.toJSON());
+        var bytes = buffer.toJSON();
+
+        // Node 0.12.x
+        if (bytes.hasOwnProperty('data')) {
+            bytes = bytes.data;
+        }
+
+        var message = ndef.decodeMessage(bytes);
+
         console.log("Found NDEF message with " + message.length +
             (message.length === 1 ? " record" : " records" ));
-        console.log(ndef.stringify(message));            
+        console.log(ndef.stringify(message));
     }
 })
-
