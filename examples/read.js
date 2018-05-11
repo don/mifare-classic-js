a018741eaba0aa532d43785e7206e3334e32cb1e
Don Coleman <don.coleman@gmail.com>
2018-05-11 18:46:48 -0400

    pass Buffer to ndef.decodeMessage requires ndef@0.2.0

--- examples/read.js
+++ examples/read.js
@@ -8,11 +8,9 @@ mifare.read(function(err, buffer, uid) {
         console.log("Read failed ");
         console.log(err);
     } else {
-        // decodeMessage expects and array of bytes e.g. [0x03, 0x13, 0xd1]
-        var bytes = buffer.toJSON().data;
-        var message = ndef.decodeMessage(bytes);
+        var message = ndef.decodeMessage(buffer);
 
-        console.log('Tag UID is', uid);
+        console.log('\nTag UID is', uid);
         console.log("Found NDEF message with " + message.length +
             (message.length === 1 ? " record" : " records" ));
         console.log(ndef.stringify(message));
